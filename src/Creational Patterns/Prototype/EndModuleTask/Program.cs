41c1af26c961f4ed07dc73e22862366c256fac05
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-03-17 11:45:09 +0200

    Prototype pattern

---
+++ src/Creational Patterns/Prototype/EndModuleTask/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace EndModuleTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Point
+    {
+        public int X, Y;
+    }
+
+    public class Line
+    {
+        public Point Start;
+        public Point End;
+
+        public Line DeepCopy()
+        {
+            var newStart = new Point { X = Start.X, Y = Start.Y };
+            var newEnd = new Point { X = End.X, Y = End.Y };
+            return new Line { Start = newStart, End = newEnd };
+        }
+    }
+}
---
+++ src/Creational Patterns/Prototype/TheBadExample/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace TheBadExample
+{
+    //This pattern is all about object copy
+    //If we have already definied complicated type and then we want to make variations of this complicated object
+    //In order to make such variations we make copy (clone) the prototype and customize it
+
+
+    public class Person : ICloneable
+        //the problem with IClonable is that we dont know if it is deep copy or shallow copy
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person(string[] names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public object Clone()
+        {
+            return new Person(Names, Address); //shallow copy
+            //So IClonable is not the right interface for deep copy
+        }
+    }
+
+    public class Address
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new[] { "John", "Smith" },
+                new Address("Street name", 123));
+
+            //now we would want to copy this person
+            //var secondPerson = person; // this wont work - we copy the refference only
+
+
+        }
+    }
+}
