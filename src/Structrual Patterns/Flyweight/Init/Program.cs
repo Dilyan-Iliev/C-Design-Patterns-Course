d9c7fb8079a7995acd0af10a268794a3520aae58
Dilyan Iliev <102140524+Dilyan-Iliev@users.noreply.github.com>
2024-04-07 12:35:28 +0300

    Flyweight pattern

---
+++ src/Structrual Patterns/Flyweight/Init/Program.cs
@@ -0,0 +1,49 @@
+﻿
+namespace Init
+{
+    public class User
+    {
+        private string fullName;
+
+        public User(string fullName)
+        {
+            this.fullName = fullName;
+        }
+    }
+
+    public class User2
+    {
+        private static List<string> strings = new();
+        private int[] names;
+
+        public User2(string fullName)
+        {
+            int getOrAdd(string s)
+            {
+                int idx = strings.IndexOf(s);
+
+                if (idx != -1) return idx;
+                else
+                {
+                    strings.Add(s);
+                    return strings.Count - 1;
+                }
+            }
+
+            names = fullName.Split(' ').Select(getOrAdd).ToArray();
+        }
+
+        public string FullName => string.Join(' ',
+            names.Select(i => strings[i]));
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //The idea of this pattern is space optimization - amount of memory that the app takes up
+            //Flyweight pattern - a space optimization technique that lets us use less memory by storing externally the data associated with similar objects.
+
+        }
+    }
+}
---
+++ src/Structrual Patterns/Flyweight/TextFormatting/Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+
+namespace TextFormatting
+{
+    public class FormattedText
+    {
+        private readonly string plainText;
+        private bool[] capitalize;
+
+        public FormattedText(string plainText)
+        {
+            this.plainText = plainText;
+            capitalize = new bool[plainText.Length];
+        }
+
+        public void Capitalize(int from, int to)
+        {
+            for (int i = from; i < to; i++)
+            {
+                capitalize[i] = true;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                var c = plainText[i];
+                sb.Append(capitalize[i] ? char.ToUpper(c) : c);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+    public class BetterFormattedText
+    {
+        private readonly string plainText;
+        private List<TextRange> formatting = new();
+
+        public BetterFormattedText(string plainText)
+        {
+            this.plainText = plainText;
+        }
+
+        public TextRange GetRange(int start, int end)
+        {
+            var range = new TextRange { Start = start, End = end };
+            formatting.Add(range);
+            return range;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                char c = plainText[i];
+
+                foreach (var range in formatting)
+                {
+                    if (range.Covers(i) && range.Capitalize)
+                    {
+                        c = char.ToUpper(c);
+                    }
+                }
+
+                sb.Append(c);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public class TextRange
+        {
+            public int Start, End;
+            public bool Capitalize;
+
+            public bool Covers(int position)
+            {
+                return position >= Start && position <= End;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ft = new FormattedText("the input text");
+            ft.Capitalize(4, 9);
+            Console.WriteLine(ft.ToString());
+
+            //----------------------------
+
+            var betterFormatting = new BetterFormattedText("the second input text");
+            betterFormatting.GetRange(4, 9).Capitalize = true;
+            Console.WriteLine(betterFormatting.ToString());
+        }
+    }
+}
